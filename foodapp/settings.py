21c1a6aa5f40361350168ef3aaeb217b8b4dc251
Blessy <blessysusancheriyan28@gmail.com>
2023-03-01 19:10:45 +0530

    add

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
+    
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'assests')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
